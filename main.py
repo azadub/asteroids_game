5616005605f4b1b649653f04aed702c5ea75c7ef
azadub <a.witten94@gmail.com>
2024-09-22 13:27:54 +1200

    C: added the triangle to the screen

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()   
     dt = 0     
-    player = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         
     while 1:        
         for event in pygame.event.get():
@@ -17,10 +17,12 @@ def main():
                 return
             
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
     
         dt = clock.tick(60) / 1000
-        
+
+              
 
 if __name__ == "__main__":
     main()
