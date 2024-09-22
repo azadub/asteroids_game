4901782f7bc616298e9f4b9f1cce005836cd08aa
azadub <a.witten94@gmail.com>
2024-09-22 13:08:17 +1200

    C: added player file

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 # https://www.pygame.org/docs/ref/pygame.html
 # https://www.pygame.org/docs/ref/surface.html#pygame.Surface.fill
-from asyncio import constants
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-            
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1:
-        pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
+    clock = pygame.time.Clock()   
+    dt = 0     
+    player = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        
+    while 1:        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    print("Starting asteroids!")
+            
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+    
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
 
-print(f"Screen width: {constants.SCREEN_WIDTH}") 
-print(f"Screen height: {constants.SCREEN_HEIGHT}") 
